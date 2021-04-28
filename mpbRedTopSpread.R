b099f24566913efa06a97cd857a6fabcd75efda0
Eliot McIntire <eliotmcintire@gmail.com>
2021-04-28 14:15:27 -0700

    streamlining -- use `dispersal2(...`

--- mpbRedTopSpread.R
+++ mpbRedTopSpread.R
@@ -73,7 +73,12 @@ doEvent.mpbRedTopSpread <- function(sim, eventTime, eventType, debug = FALSE) {
       sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "mpbRedTopSpread", "save", .last())
     },
     "dispersal" = {
-      sim <- dispersal(sim)
+      out <- dispersal2(pineMap = sim$pineMap, studyArea = sim$studyArea,
+                                     massAttacksDT = sim$massAttacksDT,
+                                     currentAttacks = sim$currentAttacks,
+                                     params = P(sim)
+      )
+      # sim <- dispersal(sim)
       sim <- scheduleEvent(sim, time(sim) + 1, "mpbRedTopSpread", "dispersal", eventPriority = 4.5)
     },
     "plot" = {
@@ -115,6 +120,10 @@ doEvent.mpbRedTopSpread <- function(sim, eventTime, eventType, debug = FALSE) {
 
 ### initilization
 Init <- function(sim) {
+  cr <- compareRaster(sim$rasterToMatch, sim$currentAttacks, sim$pineMap, orig = TRUE)
+  if (!isTRUE(cr))
+    stop("Rasters sim$rasterToMatch, sim$currentAttacks, sim$pineMap must all have same metadata; they do not")
+
   return(invisible(sim))
 }
 
@@ -131,15 +140,22 @@ plotFn <- function(sim) {
 ### spread
 dispersal <- function(sim) {
   ## check that MPB and pine rasters are the same resolution and ncells
-  stopifnot(compareRaster(sim$rasterToMatch, sim$currentAttacks, sim$pineMap, orig = TRUE))
+  if (fromDisk(sim$pineMap))
+    sim$pineMap[] <- sim$pineMap[]
 
   ## use 1125 trees/ha, per Whitehead & Russo (2005), Cooke & Carroll (2017)
   MAXTREES <- 1125 * prod(res(sim$pineMap)) / 100^2 ## TODO: round this?
 
   ## asymmetric spread (biased eastward)
   # lodgepole pine and jack pine together
-  propPineMap <- sum(sim$pineMap) / 100 ## much faster than calc; ## TODO: allow different params per species
-  propPineMap[is.na(propPineMap[])] <- 0
+  propPineMap <- if (nlayers(sim$pineMap) > 1) sum(sim$pineMap) else sim$pineMap
+  # mv <- maxValue(propPineMap)
+  #
+  # if (mv > 1)
+  #   propPineMap[] <- propPineMap[] / 100 ## much faster than calc; ## TODO: allow different params per species
+
+  nas <- is.na(propPineMap[])
+  propPineMap[nas] <- 0
   if (exists("EliotTesting")) {
     EliotTesting <- TRUE
     sim@params$mpbRedTopSpread$bgSettlingProp <- 0.7
@@ -149,7 +165,7 @@ dispersal <- function(sim) {
   } else {
     EliotTesting <- FALSE
   }
-  propPineMap[] <- pmin(1, propPineMap[] / 100 + P(sim)$bgSettlingProp) ## TODO: why divide by 100??
+  #propPineMap[] <- pmin(1, propPineMap[] + P(sim)$bgSettlingProp) ## TODO: why divide by 100??
 
   if (EliotTesting) { # TODO -- delete EliotTesting when no longer desired
     a <- extent(sim$studyArea)
@@ -163,23 +179,24 @@ dispersal <- function(sim) {
     saveStack <- raster::rasterTmpFile()
   } else {
     minNumAgents <- 50
-    starts <- sim$massAttacksDT[ATKTREES > 0][["ID"]]
+    starts <- sim$massAttacksDT[["ID"]][sim$massAttacksDT$ATKTREES > 0]
     saveStack <- NULL
     currentAttacks <- sim$currentAttacks
   }
 
-  st1 <- system.time({
-    out <- SpaDES.tools::spread3(start = starts,
-                                 rasQuality = propPineMap,
-                                 rasAbundance = currentAttacks,
-                                 advectionDir = P(sim)$advectionDir,
-                                 advectionMag = P(sim)$advectionMag,
-                                 meanDist = P(sim)$meanDist,
-                                 plot.it = !is.na(P(sim)$.plotInitialTime),
-                                 minNumAgents = minNumAgents,
-                                 verbose = 2,
-                                 saveStack = saveStack) ## saveStack is the filename to save to
-  })
+  #  st1 <- system.time({
+  out <- SpaDES.tools::spread3(start = starts,
+                               rasQuality = propPineMap,
+                               rasAbundance = currentAttacks,
+                               advectionDir = P(sim)$advectionDir,
+                               advectionMag = P(sim)$advectionMag,
+                               meanDist = P(sim)$meanDist,
+                               plot.it = !is.na(P(sim)$.plotInitialTime),
+                               minNumAgents = minNumAgents,
+                               verbose = 2,
+                               skipChecks = TRUE,
+                               saveStack = saveStack) ## saveStack is the filename to save to
+  #})
   if (EliotTesting) {
     fname <- file.path(outputPath(sim), paste0("spread", current(sim)$eventTime, ".tif"))
     tf <- raster::rasterTmpFile()
@@ -188,6 +205,25 @@ dispersal <- function(sim) {
     writeRaster(r2, fname, overwrite = TRUE)
     unlink(saveStack, tf)
   }
+
+  if (FALSE) {
+    atks <- simOut$massAttacksDT
+    nPix <- atks[ATKTREES > 0, .N]
+    atkAreaSim <- nPix * prod(res(simOut$rasterToMatch)) / (100^2) ## area in ha
+
+    ## attacked area from data
+    atksRas <- simOut$massAttacksMap[[paste0("X", timesFit$end)]]
+    atks <- data.table(ID = 1L:ncell(atksRas), ATKTREES = atksRas[])
+    nPix <- atks[ATKTREES > 0, .N] ## total number of pixels
+    atkAreaData <- nPix * prod(res(simOut$rasterToMatch)) / (100^2) ## area in ha
+
+    ## sum negative log likelihood for attacked pixels
+
+
+    ## TODO: something other than simple sum of squares?
+    metric <- (atkAreaData - atkAreaSim)^2 #+ (SNLL / 10^3)
+  }
+
 
   # if (EliotTesting) {
   #   tmpStackObj <- stack(saveStack)
@@ -210,4 +246,114 @@ dispersal <- function(sim) {
 
   # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
+}
+
+dispersal2 <- function(pineMap, studyArea, massAttacksDT, currentAttacks, params) {
+  ## check that MPB and pine rasters are the same resolution and ncells
+  if (fromDisk(pineMap))
+    pineMap[] <- pineMap[]
+
+  ## use 1125 trees/ha, per Whitehead & Russo (2005), Cooke & Carroll (2017)
+  MAXTREES <- 1125 * prod(res(pineMap)) / 100^2 ## TODO: round this?
+
+  ## asymmetric spread (biased eastward)
+  # lodgepole pine and jack pine together
+  propPineMap <- if (nlayers(pineMap) > 1) sum(pineMap) else pineMap
+  # mv <- maxValue(propPineMap)
+  #
+  # if (mv > 1)
+  #   propPineMap[] <- propPineMap[] / 100 ## much faster than calc; ## TODO: allow different params per species
+
+  nas <- is.na(propPineMap[])
+  propPineMap[nas] <- 0
+  if (exists("EliotTesting")) {
+    EliotTesting <- TRUE
+    sim@params$mpbRedTopSpread$bgSettlingProp <- 0.7
+    sim@params$mpbRedTopSpread$advectionMag <- 20000
+    minNumAgents <- 200
+    sim@params$mpbRedTopSpread$.plotInitialTime <- NA
+  } else {
+    EliotTesting <- FALSE
+  }
+  #propPineMap[] <- pmin(1, propPineMap[] + P(sim)$bgSettlingProp) ## TODO: why divide by 100??
+
+  if (EliotTesting) { # TODO -- delete EliotTesting when no longer desired
+    a <- extent(studyArea)
+    starts <- massAttacksDT[ATKTREES > 0]$ID
+    d <- raster(currentAttacks)
+    d[starts] <- 1
+    d <- crop(d, a)
+    starts <- which(d[] > 0)
+    currentAttacks <- crop(currentAttacks, a) * 300
+    propPineMap <- crop(propPineMap, a)
+    saveStack <- raster::rasterTmpFile()
+  } else {
+    minNumAgents <- 50
+    starts <- massAttacksDT[["ID"]][massAttacksDT$ATKTREES > 0]
+    saveStack <- NULL
+    currentAttacks <- currentAttacks
+  }
+
+  #  st1 <- system.time({
+  out <- SpaDES.tools::spread3(start = starts,
+                               rasQuality = propPineMap,
+                               rasAbundance = currentAttacks,
+                               advectionDir = params$advectionDir,
+                               advectionMag = params$advectionMag,
+                               meanDist = params$meanDist,
+                               plot.it = !is.na(params$.plotInitialTime),
+                               minNumAgents = minNumAgents,
+                               verbose = 2,
+                               skipChecks = TRUE,
+                               saveStack = saveStack) ## saveStack is the filename to save to
+  #})
+  if (EliotTesting) {
+    fname <- file.path(outputPath(sim), paste0("spread", current(sim)$eventTime, ".tif"))
+    tf <- raster::rasterTmpFile()
+    r <- stack(saveStack)
+    r2 <- calc(r, sum, filename = tf, overwrite = TRUE)
+    writeRaster(r2, fname, overwrite = TRUE)
+    unlink(saveStack, tf)
+  }
+
+  if (FALSE) {
+    atks <- simOut$massAttacksDT
+    nPix <- atks[ATKTREES > 0, .N]
+    atkAreaSim <- nPix * prod(res(simOut$rasterToMatch)) / (100^2) ## area in ha
+
+    ## attacked area from data
+    atksRas <- simOut$massAttacksMap[[paste0("X", timesFit$end)]]
+    atks <- data.table(ID = 1L:ncell(atksRas), ATKTREES = atksRas[])
+    nPix <- atks[ATKTREES > 0, .N] ## total number of pixels
+    atkAreaData <- nPix * prod(res(simOut$rasterToMatch)) / (100^2) ## area in ha
+
+    ## sum negative log likelihood for attacked pixels
+
+
+    ## TODO: something other than simple sum of squares?
+    metric <- (atkAreaData - atkAreaSim)^2 #+ (SNLL / 10^3)
+  }
+
+
+  # if (EliotTesting) {
+  #   tmpStackObj <- stack(saveStack)
+  #   ex <- extent(tmpStackObj)
+  #   ex@ymax <- ex@ymax - 4000
+  #   ex@ymin <- 7389000
+  #   ex@xmax <- -901000
+  #   out2 <- crop(tmpStackObj, ex) * 10
+  #   if (require(animation)) {
+  #     gifName <- "C:\\Eliot\\Google Drive\\McIntire-lab\\figures\\MPB animation 700px small area 3.gif"
+  #     #gifName <- file.path(tempdir(), "animation.gif")
+  #     saveGIF(interval = 0.1, ani.height = 700, ani.width = 700, movie.name = gifName, expr = {
+  #       for (i in seq(numLayers(out2))) plot(out2[[i]])
+  #     })
+  #   }
+  #   stop("End it here")
+  # }
+  migrantsDT <- out[, list(NEWATKs = sum(abundSettled)), by = "pixels"]
+  out2 <- massAttacksDT[migrantsDT, on = c(ID = "pixels")]
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible())
 }
