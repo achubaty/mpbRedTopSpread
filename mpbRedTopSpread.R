1e0b47e8b886c18fda9e6b3811a41753cb0136c7
Eliot McIntire <eliotmcintire@gmail.com>
2022-02-28 21:45:56 -0800

    bump version of LandR to -- found clusterSetup

--- mpbRedTopSpread.R
+++ mpbRedTopSpread.R
@@ -22,7 +22,7 @@ defineModule(sim, list(
                   "CircStats", "data.table", "DEoptim", "EnvStats",
                   "gamlss", "ggplot2", "gt",
                   "PredictiveEcology/SpaDES.core@development (>= 1.0.8.9011)",
-                  "PredictiveEcology/LandR@cluster (>= 1.0.4.9022)",
+                  "PredictiveEcology/LandR@cluster2 (>= 1.0.7.9007)",
                   "parallelly",
                   "PredictiveEcology/pemisc@development (>= 0.0.3.9001)",
                   "PredictiveEcology/mpbutils (>= 0.1.3.9000)", "purrr",
